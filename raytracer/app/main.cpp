0c5350031ae89e80ec4b0eb4da8f4f9ab69b1f99
Anton Moyseychuk <moyseychuk2017@mail.ru>
2023-04-27 15:33:51 +0300

    fix: перенес математические константы в отдельный файл

--- raytracer/app/main.cpp
+++ raytracer/app/main.cpp
@@ -3,7 +3,14 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    app::Application app("Raytracer", 640, 480);
-    app.Run();
+    // app::Application app("Raytracer", 640, 480);
+    // app.Run();
+
+    const auto q = (math::Quaternion(cos(math::ToRadians(45.0f)), 0.0f, 0.0f, cos(math::ToRadians(45.0f))));
+    const math::vec3f v = math::VECTOR_UP;
+    const auto rotated = v * q;
+
+    std::cout << "v: (" << v.x << ", " << v.y << ", " << v.z << ")\n";
+    std::cout << "rotated: (" << rotated.x << ", " << rotated.y << ", " << rotated.z << ")\n";
     return 0;
 }
