8515fea07d18e81d2f86618bb17f803b42b41ea8
Anton Moyseychuk <moyseychuk2017@mail.ru>
2023-04-29 12:39:31 +0300

    fix: настроил проект растеризатора

--- rasterizer/app/application.cpp
+++ rasterizer/app/application.cpp
@@ -20,8 +20,6 @@ namespace app {
 
         vec2ui last_window_size(m_window->GetWidth(), m_window->GetHeight()), curr_window_size = last_window_size;
 
-        std::vector<std::uint32_t> back_buffer(last_window_size.x * last_window_size.y);
-
         while (m_window->IsOpen()) {
             const auto curr_frame = std::chrono::steady_clock::now();
             const auto dt = std::chrono::duration<float>(curr_frame - m_last_frame).count();
@@ -29,15 +27,10 @@ namespace app {
 
             m_window->PollEvent();
 
-            curr_window_size = vec2ui(m_window->GetWidth(), m_window->GetHeight());
-            if (curr_window_size != last_window_size) {
-                back_buffer.resize(curr_window_size.x * curr_window_size.y);
-                last_window_size = curr_window_size;
+            for (size_t i = 0; i< m_window->GetHeight(); ++i) {
+                m_window->SetPixelColor(i, i, 255, 0, 0, 255);
             }
-
-
-            std::fill_n(back_buffer.begin(), curr_window_size.x * curr_window_size.y, Color::YELLOW.rgba);
-            m_window->FillPixelBuffer(back_buffer);
+            
             m_window->PresentPixelBuffer();          
             
             std::cout << "FPS: " << std::to_string(1.0f / dt) << std::endl;
--- rasterizer/app/main.cpp
+++ rasterizer/app/main.cpp
@@ -1,7 +1,7 @@
 #include "application.hpp"
 
 int main(int argc, char* argv[]) {
-    std::unique_ptr<app::Application> app = std::make_unique<app::Application>("rasterizer", 540, 480);
+    std::unique_ptr<app::Application> app = std::make_unique<app::Application>("rasterizer", 720, 640);
     app->Run();
     return 0;
 }
